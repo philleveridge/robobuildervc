74462e190b7f4638986cc5abc6520a746e92484a
phil.leveridge@gmail.com <phil.leveridge@gmail.com@00eb498c-0676-11de-85a1-fd18384f7567>
2011-10-22 10:04:04 +0000

    octvave module

---
+++ trunk/octave/robobuilder.h
@@ -0,0 +1,45 @@
+extern int dbg;
+extern char devicename[20];
+
+typedef struct wckResponse {
+	int b1; int b2;
+} wck_t;
+
+typedef struct result {
+	int x; int y; int z;
+} result_t;
+
+typedef struct servos {
+	int nos;
+	unsigned char values[32];
+} servos_t;
+
+typedef struct lights {
+	int value;
+	int range[5];
+} lights_t;
+
+void 		setdevname	(char *n);
+
+void 		initserial	(int f);
+int 		openport	(char *device, int baudrate);
+void 		closeport	();
+
+void  		blights		(lights_t l);
+
+int   		wckPosRead	(char ServoID);
+int  		readPSD		();   
+int 		readIR		();
+void 		standup 	(int n);
+void 		setdh		(int n);
+void 		PlayMotion	(int n);
+void 		wckWriteIO	(unsigned char ServoID, unsigned char IO);
+
+result_t	*readXYZ	();
+wck_t   	*wckReadPos	(int, int);
+wck_t 		*wckMovePos	(int id, int pos, int torq);
+wck_t 		*wckPassive	(int id);
+
+void 		SyncPosSend	(int SpeedLevel, servos_t s);
+void 		PlayPoseA	(int d, int f, int tq, int flag, servos_t s);
+servos_t 	*readservos	(int n);
